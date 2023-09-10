9065d12128834ef5c7bcbc14569c9e403fff5c1a
saina <sainagarjuna184@gmail.com>
2023-09-10 12:19:58 +0530

    added

--- frontend.sh
+++ frontend.sh
@@ -1,26 +1,26 @@
 echo installing nginx
-dnf install nginx -y > /tmp/expense/log
+dnf install nginx -y > /tmp/expense.log
 echo placing expenselog file
 
-cp expense.conf /etc/nginx/default.d/expense.conf /tmp/expense/log
+cp expense.conf /etc/nginx/default.d/expense.conf /tmp/expense.log
 
 echo enable nginx
-systemctl enable nginx /tmp/expense/log
+systemctl enable nginx /tmp/expense.log
 echo starting nginx
-systemctl start nginx /tmp/expense/log
+systemctl start nginx /tmp/expense.log
 echo deleting old content
-rm -rf /usr/share/nginx/html/* /tmp/expense/log
+rm -rf /usr/share/nginx/html/* /tmp/expense.log
 
 
 echo downloading frontend code
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip /tmp/expense/log
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip /tmp/expense.log
 
 
-cd /usr/share/nginx/html /tmp/expense/log
+cd /usr/share/nginx/html /tmp/expense.log
 echo extracting frontend code
-unzip /tmp/frontend.zip /tmp/expense/log
+unzip /tmp/frontend.zip /tmp/expense.log
 
 
 
 
-systemctl restart nginx /tmp/expense/log
+systemctl restart nginx /tmp/expense.log
